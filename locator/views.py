c0e8cb811e24e6e2137208dcc560d3f38b1a8a77
Isaac Bythewood <isaac@bythewood.me>
2012-12-05 03:38:41 +0000

    Initial commit

---
+++ locator/views.py
@@ -0,0 +1,33 @@
+import json
+
+from django.template.response import TemplateResponse
+from django.http import HttpResponse
+
+from .models import Location
+
+
+def locator(request):
+    return TemplateResponse(request, 'locator/locator.html')
+
+
+def locations(request):
+    locations = Location.objects.all()
+    formatted_locations = []
+    count = 1
+    for location in locations:
+        formatted_locations.append({
+            'id': count,
+            'locname': location.name,
+            'lat': location.latitude,
+            'lng': location.longitude,
+            'address': location.street,
+            'city': location.city,
+            'state': location.state,
+            'postal': location.postal,
+            'phone': location.phone,
+            'web': location.website,
+            'hours1': location.hours,
+        }]
+        count += 1
+    return HttpResponse(json.dumps(formatted_locations),
+                        mimetype='application/json')
